b059fa7a7b532b596c547d382288b6aadb72e4e0
SphinX <saeedtaha.sm@gmail.com>
2019-10-16 16:48:45 +0330

    asd

---
+++ app/src/main/java/com/samlifttruck/activity/Models/CardexDetailsModel.java
@@ -0,0 +1,99 @@
+
+package com.samlifttruck.activity.Models;
+
+public class CardexDetailsModel {
+
+    private int businessID;
+    private String businessName;
+    private String businessNominal;
+    private int incoming;
+    private int onHand;
+    private int outGoing;
+    private String persianBusinessDate;
+    private String personName;
+    private String productName;
+    private String ID;
+
+    public CardexDetailsModel() {
+    }
+
+    public int getBusinessID() {
+        return businessID;
+    }
+
+    public void setBusinessID(int businessID) {
+        this.businessID = businessID;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getBusinessNominal() {
+        return businessNominal;
+    }
+
+    public void setBusinessNominal(String businessNominal) {
+        this.businessNominal = businessNominal;
+    }
+
+    public int getIncoming() {
+        return incoming;
+    }
+
+    public void setIncoming(int incoming) {
+        this.incoming = incoming;
+    }
+
+    public int getOnHand() {
+        return onHand;
+    }
+
+    public void setOnHand(int onHand) {
+        this.onHand = onHand;
+    }
+
+    public int getOutGoing() {
+        return outGoing;
+    }
+
+    public void setOutGoing(int outGoing) {
+        this.outGoing = outGoing;
+    }
+
+    public String getPersianBusinessDate() {
+        return persianBusinessDate;
+    }
+
+    public void setPersianBusinessDate(String persianBusinessDate) {
+        this.persianBusinessDate = persianBusinessDate;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
---
+++ app/src/main/java/com/samlifttruck/activity/Models/DetailsModel.java
@@ -0,0 +1,60 @@
+
+package com.samlifttruck.activity.Models;
+
+public class DetailsModel {
+
+    private String onHand;
+    private String productCode;
+    private String productName;
+    private String qty;
+    private String techNo;
+    private String unitName;
+
+    public String getOnHand() {
+        return onHand;
+    }
+
+    public void setOnHand(String onHand) {
+        this.onHand = onHand;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getTechNo() {
+        return techNo;
+    }
+
+    public void setTechNo(String techNo) {
+        this.techNo = techNo;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+}
